69ec29e0f64e9c0220be8e4249cb9891e0090779
Dmitrii Chizhov <dimachig@gmail.com>
2023-01-17 16:26:15 +0300

    fix: js errors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const aboutTabs = document.getElementsByClassName('about-tabs__tab');
 const aboutTabContent = document.getElementsByClassName('about-content');
+const progressTabs = document.getElementsByClassName('progress-tabs__tab');
+const progressTabContent = document.getElementsByClassName('progress-content');
 
 [...aboutTabs].forEach(tab => tab.addEventListener('click', tabClick));
-
+// [...progressTabs].forEach(tab => tab.addEventListener('click', tabClick));
 function tabClick(event) {
     const tabId = event.target.dataset.id;
 
@@ -13,7 +15,16 @@ function tabClick(event) {
     })
 
     aboutTabs[tabId - 1].classList.add('active');
-    aboutTabContent[tabId - 1].classList.add('active')
+    aboutTabContent[tabId - 1].classList.add('active');
+
+    // [...progressTabs].forEach((tab, i) => {
+    //     tab.classList.remove('active-tab');
+    //     progressTabContent[i].classList.remove('active-tab');
+    //
+    //
+    // progressTabs[tabId - 1].classList.add('active-tab');
+    // progressTabContent[tabId - 1].classList.add('active-tab')
+// })
 }
 
 
